ec7beb320bb3564d78929d27bafa90587087611d
Coley Brown <colebrow.cb@gmail.com>
2021-05-21 22:33:09 -0400

    Initial pass

---
+++ src/components/TodoTask.svelte
@@ -0,0 +1,185 @@
+<script>
+  export let todo;
+  export let removeTodo;
+  let editing;
+  import { tick } from 'svelte'
+
+  const onKeyPress = e => {
+    if (e.charCode === 13) editing = false;
+  };
+
+  async function focusInput(el){
+    await tick();
+    el.focus();
+  }
+</script>
+
+<li class:completed={todo.completed} class:editing={editing} on:pointerleave={() => editing = false}>
+  <div class="view">
+    <input class="toggle" type="checkbox" checked={todo.completed} on:change={() => todo.completed = !todo.completed}>
+    {#if editing}
+      <input class="edit" type="text" bind:value={todo.name} on:keypress={onKeyPress} use:focusInput>
+    {:else}
+      <label on:dblclick={() => editing = true}>{todo.name}</label>
+      <button class="destroy" on:click={() => removeTodo(todo)}></button>
+    {/if}
+  </div>
+</li>
+
+<style>
+  
+  button {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    background: none;
+    font-size: 100%;
+    vertical-align: baseline;
+    font-family: inherit;
+    font-weight: inherit;
+    color: inherit;
+    -webkit-appearance: none;
+    appearance: none;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  li {
+	position: relative;
+	font-size: 24px;
+	border-bottom: 1px solid #ededed;
+}
+
+li:last-child {
+	border-bottom: none;
+}
+
+li.editing {
+	border-bottom: none;
+	padding: 0;
+}
+
+li.editing .edit {
+	display: block;
+	width: 506px;
+	padding: 12px 16px;
+	margin: 0 0 0 43px;
+}
+
+li .toggle {
+	text-align: center;
+	width: 40px;
+	/* auto, since non-WebKit browsers doesn't support input styling */
+	height: auto;
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	margin: auto 0;
+	border: none; /* Mobile Safari */
+	-webkit-appearance: none;
+	appearance: none;
+}
+
+li .toggle {
+	opacity: 0;
+}
+
+li .toggle + label {
+	/*
+		Firefox requires `#` to be escaped - https://bugzilla.mozilla.org/show_bug.cgi?id=922433
+		IE and Edge requires *everything* to be escaped to render, so we do that instead of just the `#` - https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/7157459/
+	*/
+	background-image: url('data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23ededed%22%20stroke-width%3D%223%22/%3E%3C/svg%3E');
+	background-repeat: no-repeat;
+	background-position: center left;
+}
+
+li .toggle:checked + label {
+	background-image: url('data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23bddad5%22%20stroke-width%3D%223%22/%3E%3Cpath%20fill%3D%22%235dc2af%22%20d%3D%22M72%2025L42%2071%2027%2056l-4%204%2020%2020%2034-52z%22/%3E%3C/svg%3E');
+}
+
+li label {
+	word-break: break-all;
+	padding: 15px 15px 15px 60px;
+	display: block;
+	line-height: 1.2;
+	transition: color 0.4s;
+}
+
+li.completed label {
+	color: #d9d9d9;
+	text-decoration: line-through;
+}
+
+li .destroy {
+	display: none;
+	position: absolute;
+	top: 0;
+	right: 10px;
+	bottom: 0;
+	width: 40px;
+	height: 40px;
+	margin: auto 0;
+	font-size: 30px;
+	color: #cc9a9a;
+	margin-bottom: 11px;
+	transition: color 0.2s ease-out;
+}
+
+li .destroy:hover {
+	color: #af5b5e;
+}
+
+li .destroy:after {
+	content: '×';
+}
+
+li:hover .destroy {
+	display: block;
+}
+
+li .edit {
+	display: none;
+}
+
+li.editing:last-child {
+	margin-bottom: -1px;
+}
+
+.info {
+	margin: 65px auto 0;
+	color: #bfbfbf;
+	font-size: 10px;
+	text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
+	text-align: center;
+}
+
+.info p {
+	line-height: 1;
+}
+
+.info a {
+	color: inherit;
+	text-decoration: none;
+	font-weight: 400;
+}
+
+.info a:hover {
+	text-decoration: underline;
+}
+
+/*
+	Hack to remove background from Mobile Safari.
+	Can't use it globally since it destroys checkboxes in Firefox
+*/
+@media screen and (-webkit-min-device-pixel-ratio:0) {
+	.toggle-all,
+	.todo-list li .toggle {
+		background: none;
+	}
+
+	.todo-list li .toggle {
+		height: 40px;
+	}
+}
+</style>
